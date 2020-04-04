a1116556485ac56073e42ab9294d62fa95758b08
yaoyangwei <yaoyangwei@qq.com>
2020-04-04 14:20:26 +0800

    dd

---
+++ cd-help.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+i=0
+str=">"
+
+arr=("|" "/" "-" "\\")
+function show_process() {
+    while [[ ${i} -le $1 ]]
+    do
+    let index=i%4
+    printf "\e[0;$2;1m[%-100s][%d%%]%c\e[0m %s\r" "$str" "$i" "${arr[${index}]}" "$4"
+    sleep $3
+    let i++
+    str='='${str}
+    done
+    if [[ $1 -eq 100 ]]; then
+        printf "\e[0m\n"
+    fi
+}
